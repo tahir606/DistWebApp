a9f7bc38628e9a13a2ce5eb606c7564023d10baf
Tahir Shakir <tahir60652@gmail.com>
2018-10-30 06:57:12 -0700

    lol

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -4,7 +4,7 @@
     var appMy = angular.module('mainPage');
 
     appMy.controller('mainCtrl', function ($scope, $http) {
-
+        $scope.sampleData = "Sample Data";
     });
 
     appMy.directive("fileReader", function () {
@@ -16,19 +16,34 @@
                 $(element).on('change', function (changeEvent) {
                     var files = changeEvent.target.files;
                     if (files.length) {
-                        var r = new FileReader();
-                        r.onload = function (e) {
-                            var contents = e.target.result;
-                            scope.$apply(function () {
-                                scope.fileReader = contents;
+                        
+                        readFile(files[0], function (content) {
+                            var data = content.split("\n");
+                            data.forEach(function (t) {
+                                var item = t.split(";");
+                                console.log(item);
+                                scope.fileReader = "Wow what";
                             });
-                        }
-                        r.readAsBinaryString(files[0]);
 
+                        });
                     }
                 });
             }
         };
     });
+
+    function readFile(file, cb) { // We pass a callback as parameter
+        var content = "";
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            content = reader.result;
+            // Content is ready, call the callback
+            cb(content);
+        }
+
+        reader.readAsText(file);
+        // return content; This is not needed anymore
+    }
 
 })();
